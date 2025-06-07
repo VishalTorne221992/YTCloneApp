84e6bf9a0c0de6804d26a40574ab79a1c9b77a49
Vishal Torne <43705835+VishalTorne221992@users.noreply.github.com>
2025-06-07 19:22:29 +0530

    Update server.js
    extended body json limit

--- YTBackend/server.js
+++ YTBackend/server.js
@@ -19,7 +19,7 @@ config();
 
 // use neccessary packages for parsing json, cookie and using session in the app. 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({limit : '50mb'}));
 app.use(express.urlencoded({extended : false}))
 app.use((req, res, next) => {
     console.log(req.path, req.method)
